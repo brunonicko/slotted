75a0e15c9a53776aae3612546879b8fdfefbd0e1
Bruno Nicko <brunonicko@gmail.com>
2020-03-22 17:10:04 -0700

    add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/brunonicko/slotted",
     packages=setuptools.find_packages(),
+    install_requires=["six", "typing"],
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
