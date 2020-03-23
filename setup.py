da1c50a7dcd585c09da633135b658ab5d72b4b36
Bruno Nicko <brunonicko@gmail.com>
2020-03-23 11:20:09 -0700

    version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="slotted",
-    version="0.0.1",
+    version="0.0.2",
     author="Bruno Nicko",
     author_email="brunonicko@gmail.com",
     description="Enforces usage of '__slots__' for python classes",
