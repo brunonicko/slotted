6d39d94c0b9b799125b262b619af5fa51033802e
Bruno Nicko <brunonicko@gmail.com>
2020-10-18 19:03:59 -0700

    Fix imports not working

--- slotted/__init__.py
+++ slotted/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Enforces usage of '__slots__' for python classes."""
 
-from ._abc import __all__ as _collections_all
+from ._abc import *  # noqa
+from ._abc import __all__ as _collections_all  # noqa
+from ._bases import Slotted, SlottedMeta, get_state, set_state  # noqa
 
 __all__ = ["get_state", "set_state", "SlottedMeta", "Slotted"] + _collections_all
