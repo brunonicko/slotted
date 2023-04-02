917112f402cf03ca77f70237e0e0312174bc6a2b
Bruno Nicko <brunonicko@gmail.com>
2023-04-01 20:59:46 -0700

    feat: abc classes

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="slotted",
-    version="5.0.1",
+    version="5.1.0",
     author="Bruno Nicko",
     author_email="brunonicko@gmail.com",
     description="Enforces usage of '__slots__' for python classes.",
---
+++ tests/test_abc.py
@@ -0,0 +1,101 @@
+# type: ignore
+
+from abc import ABCMeta
+
+import pytest
+import tippo
+from six.moves import collections_abc
+
+import slotted
+from slotted import _abc as slotted_abc  # noqa
+from slotted._abc import _MISSING_TYPES  # noqa
+from slotted._abc import __all__ as slotted_abc_all  # noqa
+from slotted._slotted import Slotted, SlottedMeta  # noqa
+
+collections_all = getattr(collections_abc, "__all__")
+
+T = tippo.TypeVar("T")
+KT = tippo.TypeVar("KT")
+VT = tippo.TypeVar("VT")
+
+
+def test_import_abc():
+    assert slotted_abc_all
+    for name in slotted_abc_all:
+        assert getattr(slotted, name)
+
+
+def test_converted():
+    not_converted = {"SlottedABCMeta", "SlottedABC"}
+
+    for name in set(slotted_abc_all).difference(not_converted):
+        assert name.startswith("Slotted")
+        assert hasattr(slotted_abc, name)
+
+        original_name = name[len("Slotted") :]
+        if original_name not in collections_all:
+            if original_name == "ABCGenericMeta":
+                continue
+            assert original_name in _MISSING_TYPES
+            assert not hasattr(collections_abc, original_name)
+            assert getattr(slotted_abc, name) is not None
+            continue
+
+        assert original_name in collections_all
+
+        cls = getattr(slotted_abc, name)
+        assert isinstance(cls, ABCMeta)
+        assert isinstance(cls, SlottedMeta)
+        assert issubclass(cls, Slotted)
+
+        original_cls = getattr(collections_abc, original_name)
+        assert issubclass(type(cls), type(original_cls))
+        assert issubclass(cls, original_cls)
+
+
+def test_generics():
+    generics = {
+        slotted_abc.SlottedContainer: (T,),
+        slotted_abc.SlottedItemsView: (KT, VT),
+        slotted_abc.SlottedIterable: (T,),
+        slotted_abc.SlottedIterator: (T,),
+        slotted_abc.SlottedKeysView: (T,),
+        slotted_abc.SlottedMapping: (KT, VT),
+        slotted_abc.SlottedMappingView: (T,),
+        slotted_abc.SlottedMutableMapping: (KT, VT),
+        slotted_abc.SlottedMutableSequence: (T,),
+        slotted_abc.SlottedMutableSet: (T,),
+        slotted_abc.SlottedSequence: (T,),
+        slotted_abc.SlottedSet: (T,),
+        slotted_abc.SlottedValuesView: (T,),
+        slotted_abc.SlottedCollection: (T,),
+        slotted_abc.SlottedReversible: (T,),
+    }
+    for generic_cls, type_vars in generics.items():
+        assert generic_cls[type_vars]
+
+    metaclasses = set(type(g) for g in generics)
+    assert len(metaclasses) == 1
+    assert next(iter(metaclasses)) is slotted_abc.SlottedABCGenericMeta
+
+
+def test_inheritance():
+    class Foo(slotted_abc.SlottedMapping[KT, VT]):  # noqa
+        pass
+
+    assert isinstance(Foo, SlottedMeta)
+    assert issubclass(Foo, Slotted)
+
+    class Bar(Foo):
+        pass
+
+    assert Bar
+
+    class BarGeneric(Foo[KT, VT]):  # noqa
+        pass
+
+    assert BarGeneric
+
+
+if __name__ == "__main__":
+    pytest.main()
